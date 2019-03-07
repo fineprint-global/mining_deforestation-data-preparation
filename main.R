6c8d925d028d60d7e76dc65a3a90701dac391d3a
istrauch <isabel.strauch@wu.ac.at>
2019-03-07 13:23:39 +0100

    calculate deforestation area grouped by year for small test sample of mines

--- main.R
+++ main.R
@@ -5,7 +5,7 @@ library(units)
 
 # Read mine locations and deforestation layer
 mines <- sf::st_read("./input/snl_samples.shp")
-deforestation <- raster::raster("./input/Hansen_GFC-2017-v1.5_lossyear_00N_060W.tif")
+deforestation_year <- raster::raster("./input/Hansen_GFC-2017-v1.5_lossyear_00N_060W.tif")
 view(mines)
 view(deforestation)
 
@@ -13,41 +13,53 @@ view(deforestation)
 image(deforestation)
 plot(mines, add = TRUE, col = "black")
 
-# Reproject vector and raster
-mines_reproject <- sf::st_transform(mines, crs = "+proj=robin")
-deforestation_reproject <- deforestation %>% raster::projectRaster(crs = "+proj=robin", 
-                                                 method = "ngb")
-view(mines_reproject)
+# Reproject vector (and raster/not needed)
+mines_projected <- sf::st_transform(mines, crs = "+proj=robin")
+#deforestation_year_reproject <- deforestation_year %>% raster::projectRaster(crs = "+proj=robin", method = "ngb")
+view(mines_projected)
 
 #######################################################################################
 # Plot reprojected data
-image(deforestation_reproject)
-plot(mines_reproject, add = TRUE, col = "black")
+image(deforestation__year_reproject)
+plot(mines_projected, add = TRUE, col = "black")
 
 # Create Buffer Areas of mines (20 km) and merge overlapping areas; trace back to mines
 buffer <- units::set_units(20, km)
-mines_reproject_buffer <- sf::st_buffer(mines_reproject, dist = buffer, nQuadSegs = 30, endCapStyle = "ROUND",
-                                  joinStyle = "ROUND", mitreLimit = 1) %>%  
+mines_projected_buffer <- sf::st_buffer(mines_projected, dist = buffer, nQuadSegs = 30, endCapStyle = "ROUND",
+                                        joinStyle = "ROUND", mitreLimit = 1) %>%  
   sf::st_union() %>%  
   sf::st_cast("POLYGON")  %>% 
   sf::st_sf() %>% 
   dplyr::mutate(id_buffer = 1:n())
 
-view(st_join(mines_reproject, mines_reproject_buffer, join = st_intersects))
+mines_projected_buffer <- st_join(mines_projected_buffer,mines_projected, join = st_intersects)
 
-mines_buffer_test <- mines_reproject %>% 
-  dplyr::mutate(year = raster::extract(deforestation_reproject, as(mines_reproject, "Spatial")))
-mines_buffer_test
-view(mines_buffer_test)
+# Unproject mines
+mines_unprojected_buffer <- sf::st_transform(mines_projected_buffer, crs = "+proj=longlat")
 
-mines_buffer_test <- mines_reproject_buffer %>% 
-  dplyr::mutate(year = raster::extract(deforestation_reproject, as(mines_reproject_buffer, "Spatial")))
+# Calculating area of raster pixel of deforestation file
+pixel_area <- raster::area(deforestation_year)
+deforestation_area <- raster::stack(list(year = deforestation_year, area = pixel_area))
+deforestation_area
 
-mines_buffer_test$year[[1]]
-view(mines_buffer_test)
+# Extract values from raster
+mines_unprojected_buffer <- mines_unprojected_buffer %>% 
+  dplyr::mutate(raster_values = raster::extract(deforestation_area, as(mines_unprojected_buffer, "Spatial")))
+
+# calculate area using pixel area
+mines_unprojected_buffer <- mines_unprojected_buffer %>% 
+  dplyr::mutate(raster_values = lapply(raster_values, tibble::as_tibble)) %>% 
+  tidyr::unnest() %>% 
+  dplyr::filter(year > 0) %>% 
+  dplyr::group_by(id_buffer, year) %>% 
+  dplyr::summarise(n_px = n(), area = sum(area, na.rm = TRUE)) 
 
 
-mines_buffer_test <- mines %>% 
-  dplyr::mutate(year = raster::extract(deforestation_year, as(mines, "Spatial"), buffer = 500))
+# Aggregate deforestation by year
+mines_deforestation_year <- mines_unprojected_buffer %>% 
+  dplyr::group_by(year) %>% 
+  dplyr::summarise(area = sum(area, na.rm = TRUE)) 
 
+# Save data to file
+sf::st_write(mines_deforestation_year, ("./output/snl_samples_deforestation.shp"))
 
