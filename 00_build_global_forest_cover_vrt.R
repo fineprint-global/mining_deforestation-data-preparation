9a5187854a2c0542a1ee6bfe2edd1517ee108144
vmaus <vwmaus1@gmail.com>
2021-03-01 12:23:35 +0100

    Clean and add README

--- 00_build_global_forest_cover_vrt.R
+++ 00_build_global_forest_cover_vrt.R
@@ -5,28 +5,28 @@ raster::rasterOptions(tmpdir = "./raster_tmp/")
 raster::rasterOptions(format = "GTiff")
 
 # --------------------------------------------------------------------------------------
-# set path to data sets 
-data_path <- "/gpfs/home/home/vmaus/data/geoserver"
+# set path to data sets
+data_path <- paste0(Sys.getenv("HOME"),"/data/geoserver")
 
 # --------------------------------------------------------------------------------------
-# get path dir data sets 
+# get path dir data sets
 pixel_area_dir <- path.expand(paste0(data_path, "/hansen/pixel_area_new"))
 forest_loss_dir <- path.expand(paste0(data_path, "/hansen/v1.7/lossyear"))
 treecover2000_dir <- path.expand(paste0(data_path, "/hansen/v1.7/treecover2000"))
 
 # --------------------------------------------------------------------------------------
-# create global VRT 
+# create global VRT
 pixel_area_vrt <- stringr::str_glue("{pixel_area_dir}/area.vrt")
-gdalUtils::gdalbuildvrt(gdalfile = dir(pixel_area_dir, pattern = ".tif$", full.names = TRUE), 
+gdalUtils::gdalbuildvrt(gdalfile = dir(pixel_area_dir, pattern = ".tif$", full.names = TRUE),
                         te = c(-180, -90, 180, 90),
                         output.vrt = pixel_area_vrt)
 
 forest_loss_vrt <- stringr::str_glue("{forest_loss_dir}/lossyear.vrt")
-gdalUtils::gdalbuildvrt(gdalfile = dir(forest_loss_dir, pattern = ".tif$", full.names = TRUE), 
+gdalUtils::gdalbuildvrt(gdalfile = dir(forest_loss_dir, pattern = ".tif$", full.names = TRUE),
                         te = c(-180, -90, 180, 90),
                         output.vrt = forest_loss_vrt)
 
 treecover2000_vrt <- stringr::str_glue("{treecover2000_dir}/treecover2000.vrt")
-gdalUtils::gdalbuildvrt(gdalfile = dir(treecover2000_dir, pattern = ".tif$", full.names = TRUE), 
+gdalUtils::gdalbuildvrt(gdalfile = dir(treecover2000_dir, pattern = ".tif$", full.names = TRUE),
                         te = c(-180, -90, 180, 90),
                         output.vrt = treecover2000_vrt)
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# mining_deforestation
-analysis of deforestation in buffer zones of mines
+# Mining & Deforestation: data preparation 
+
+The scripts in this repository produces the data for the statistical analysis <https://github.com/fineprint-global/mining_deforestation-stat> in *Mining is a major driver of deforestation across tropical forests* by Giljum et al. (Under review). 
+
+- `download_osm/ ` download and process data from  [OpenStreetMap](OpenStreetMaphttps://www.openstreetmap.org)
+- `download_urban/` download data on accessibility (Weiss et al. (2018) <https://map.ox.ac.uk/research-project/accessibility_to_cities/>) and ESA/CCI Land Cover Map for 2000 over their FTP, see <http://maps.elie.ucl.ac.be/CCI/viewer/download.php>
+- `download_soilgrids/` Download soil grids <https://files.isric.org/soilgrids/latest/data/wrb/>
+- `download_sedac/` Download population density from SEDAC <https://sedac.ciesin.columbia.edu/data/set/gpw-v4-population-density-rev11/data-download>
+- `run_jobs` send jobs to cluster to process global 30 arcsec grid
+- `run_pixel_area` send jobs to cluster to calculate pixel area 
+- `00_build_global_forest_cover_vrt.R` Create global VRT for rest data 
+- `02_calculate_distance_osm.R` compute distance to transport infrastructure 
+- `03_additional_2000_layers.R` compute distance to agriculture
+- `05_calculate_distance_to_the_sea.R` compute distance to coastline
+- `06_multiscale_mining.R` compute mining area at multiple scales 
+- `07_build_fineprint_30sec_grid.R` compute global 30 arcsec grid -- produces tiff and geojson files
+- `08_bind_timeseries.R` merge forest loss time series 
+- `R/` Supporting functions 
